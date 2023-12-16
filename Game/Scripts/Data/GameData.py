613e6981a36497b7e546129a43d72b42e8fa8084
Xdecent <xdecentwork@gmail.com>
2023-12-16 16:19:07 +0200

    some test changes

--- Game/Scripts/Data/GameData.py
+++ Game/Scripts/Data/GameData.py
@@ -1,4 +1,56 @@
+# import pickle
+
+# class GameData:
+#     _instance = None
+
+#     def __new__(cls):
+#         if cls._instance is None:
+#             cls._instance = super(GameData, cls).__new__(cls)
+#             cls._instance._initialize()
+#         return cls._instance
+
+#     def _initialize(self):
+#         self.highscore = 0
+#         self.coins = 0
+#         self.lives = 1
+#         self.player_color = "purple"
+#         self.outline_color = "black"
+#         self.name = "Player"
+
+#     def save(self):
+#         with open("./Game/Scripts/Data/game_data.pkl", "wb") as file:
+#             pickle.dump(self, file)
+
+#     def load(self):
+#         try:
+#             with open("./Game/Scripts/Data/game_data.pkl", "rb") as file:
+#                 game_data = pickle.load(file)
+#                 self.highscore = game_data.highscore
+#                 self.coins = game_data.coins
+#                 self.lives = game_data.lives
+#                 self.player_color = game_data.player_color
+#                 self.outline_color = game_data.outline_color
+#                 self.name = game_data.name
+#         except (FileNotFoundError, EOFError):
+#             self._initialize()
+
+#     def reset(self):
+#         current_name = self.name
+#         self._initialize()
+#         self.name = current_name
+
+
+
 import pickle
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.highscore = 0
+        self.coins = 0
+        self.lives = 1
+        self.player_color = "purple"
+        self.outline_color = "black"
 
 class GameData:
     _instance = None
@@ -10,12 +62,12 @@ class GameData:
         return cls._instance
 
     def _initialize(self):
-        self.highscore = 0
-        self.coins = 0
-        self.lives = 1
-        self.player_color = "purple"
-        self.outline_color = "black"
-        self.name = "Player"
+        self.players = {
+            "Player1": Player("Player1"),
+            "Player2": Player("Player2"),
+            "Player3": Player("Player3")
+        }
+        self.current_player = "Player1"
 
     def save(self):
         with open("./Game/Scripts/Data/game_data.pkl", "wb") as file:
@@ -25,16 +77,33 @@ class GameData:
         try:
             with open("./Game/Scripts/Data/game_data.pkl", "rb") as file:
                 game_data = pickle.load(file)
-                self.highscore = game_data.highscore
-                self.coins = game_data.coins
-                self.lives = game_data.lives
-                self.player_color = game_data.player_color
-                self.outline_color = game_data.outline_color
-                self.name = game_data.name
+                self.players = game_data.players
+                self.current_player = game_data.current_player
         except (FileNotFoundError, EOFError):
             self._initialize()
 
-    def reset(self):
-        current_name = self.name
-        self._initialize()
-        self.name = current_name
+    def switch_player(self, player_name):
+        if player_name in self.players:
+            self.current_player = player_name
+
+    def get_current_player_info(self):
+        return self.players[self.current_player].__dict__
+
+    def get_coins(self):
+        return self.players[self.current_player].coins
+
+    def get_highscore(self):
+        return self.players[self.current_player].highscore
+
+    def get_lives(self):
+        return self.players[self.current_player].lives
+
+    def get_name(self):
+        return self.players[self.current_player].name
+
+    def get_player_color(self):
+        return self.players[self.current_player].player_color
+
+    def get_outline_color(self):
+        return self.players[self.current_player].outline_color
+
